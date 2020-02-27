b49672d4e4fc68d2c79a4cad53ebd02490d5c00d
petromw <54507601+petromw@users.noreply.github.com>
2020-02-26 21:12:44 -0500

    Created Project

--- WhatShouldIRead/app/src/test/java/com/example/whatshouldiread/ExampleUnitTest.kt
+++ WhatShouldIRead/app/src/test/java/com/example/whatshouldiread/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isStillCorrect() {
+        assertEquals(3, 2 + 2)
+    }
 }
