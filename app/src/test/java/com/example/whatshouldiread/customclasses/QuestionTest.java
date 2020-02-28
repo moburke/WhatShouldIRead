8242e278b7000f3af46d9cee9ddf71336a78bce8
Minh-Dao Vu <vumd@mail.uc.edu>
2020-02-27 19:48:07 -0500

    Initial commit for UnitTests

---
+++ app/src/test/java/com/example/whatshouldiread/customclasses/QuestionTest.java
@@ -0,0 +1,24 @@
+package com.example.whatshouldiread.customclasses;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void subtractionWorks()
+    {
+        assertEquals(2, 4-2);
+    }
+}
