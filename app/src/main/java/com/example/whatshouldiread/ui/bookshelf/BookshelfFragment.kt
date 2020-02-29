cee85e83e6f127aace5ddf4e59ae4f098624a9db
petromw <54507601+petromw@users.noreply.github.com>
2020-02-28 19:13:26 -0500

    Added colors and pages, began to change the UI

--- app/src/main/java/com/example/whatshouldiread/ui/bookshelf/BookshelfFragment.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/bookshelf/BookshelfFragment.kt
@@ -1,28 +1,27 @@
-package com.example.whatshouldiread.ui.home
+package com.example.whatshouldiread.ui.bookshelf
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.whatshouldiread.R
 
-class HomeFragment : Fragment() {
+class BookshelfFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var bookshelfViewModel: BookshelfViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {})
+        bookshelfViewModel =
+            ViewModelProviders.of(this).get(BookshelfViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_bookshelf, container, false)
+        bookshelfViewModel.text.observe(viewLifecycleOwner, Observer {})
         return root
     }
 }
--- app/src/main/java/com/example/whatshouldiread/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/profile/ProfileFragment.kt
@@ -1,28 +1,27 @@
-package com.example.whatshouldiread.ui.home
+package com.example.whatshouldiread.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.whatshouldiread.R
 
-class HomeFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {})
+        profileViewModel =
+            ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {})
         return root
     }
 }
