0009b8ba9776926a6c1f9e78469f6af570d6f92c
Casey <caseyjr07@sbcglobal.net>
2020-03-08 15:56:01 -0400

    Added CSS file

---
+++ WhatShouldIRead/README.md
@@ -0,0 +1,8 @@
+# WhatShouldIRead
+Application that allows users to find a book to read based on a quiz of various questions. Will allow users to:
+
+1. Take a quiz to find what book they should read based on their answers.
+1. Provide users information of the book from the quiz result (Book Name, Author, Amazon Link)
+1. Allow users to create a profile to store their quiz results and view past results.
+
+Please email us before creating a branch and pull request. Use the [UC Student Directory](https://ucdirectory.uc.edu/StudentDirectory.asp) to find the emails.
---
+++ WhatShouldIRead/app/src/test/java/com/example/whatshouldiread/customclasses/BookTest.java
@@ -0,0 +1,59 @@
+package com.example.whatshouldiread.customclasses;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void bookCons()
+    {
+        Book book1 = new Book();
+        assertNotNull(book1);
+    }
+
+
+    @Test
+    public void bookObjects(){
+        Question[] bookQuestions = new Question[]{new Question()};
+
+        Book bookInfo  = new Book("TestBook","TestAuthor", bookQuestions, new int[] {1});
+        assertNotNull(bookInfo);
+    }
+
+    @Test
+    public void getMethods() {
+        Book book1 = new Book();
+
+        assertEquals(book1.getBookName(), "TestBook");
+        assertEquals(book1.getBookAuthor(), "TestAuthor");
+        assertNotNull(book1.getBookQuestions());
+        assertNotNull(book1.getAmazonStoreLink());
+        assertNotNull(book1.getBookImagePath());
+    }
+
+    @Test
+    public void setMethods()
+    {
+        Book book1 = new Book();
+
+        book1.setBookAuthor("Author");
+        book1.setBookName("Book");
+        book1.setBookQuestions(new Question[] {new Question(), new Question()});
+
+        assertNotNull(book1.getBookAuthor());
+        assertNotNull(book1.getBookName());
+        assertNotNull(book1.getBookQuestions());
+    }
+}
