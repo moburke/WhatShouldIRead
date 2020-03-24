191106d93dfb1bca467546b22f873e3df9a4e518
petromw <54507601+petromw@users.noreply.github.com>
2020-03-24 14:25:43 -0400

    Updated main activity including creation of button to start the quiz

--- app/src/main/java/com/example/whatshouldiread/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/home/MainActivity.kt
@@ -1,14 +1,16 @@
-package com.example.whatshouldiread
+package com.example.whatshouldiread.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.whatshouldiread.customclasses.Book
-import com.example.whatshouldiread.customclasses.Question
+import com.example.whatshouldiread.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +24,17 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_bookshelf, R.id.navigation_profile
+                R.id.navigation_home,
+                R.id.navigation_bookshelf,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        btn_start_quiz.setOnClickListener{
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
