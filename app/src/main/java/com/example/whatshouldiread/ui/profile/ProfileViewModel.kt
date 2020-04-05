74602e939e2c7578690aed0b41247f1e94d55b0c
Minh-Dao Vu <vumd@mail.uc.edu>
2020-04-05 19:09:09 -0400

    Revert "Code Review 2"
    This reverts commit 912c31bc1719b4bb194fdf99570cd4fb392d02ad.

--- app/src/main/java/com/example/whatshouldiread/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/profile/ProfileViewModel.kt
@@ -11,30 +11,3 @@ class ProfileViewModel : ViewModel() {
     }
     val text: LiveData<String> = _text
 }
-
-
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    ...
-    handleIntent(intent)
-}
-
-override fun onNewIntent(intent: Intent) {
-    super.onNewIntent(intent)
-    handleIntent(intent)
-}
-
-private fun handleIntent(intent: Intent) {
-    val appLinkAction = intent.action
-    val appLinkData: Uri? = intent.data
-    if (Intent.ACTION_VIEW == appLinkAction) {
-        appLinkData?.lastPathSegment?.also { recipeId ->
-            Uri.parse("content://com.recipe_app/recipe/")
-                .buildUpon()
-                .appendPath(recipeId)
-                .build().also { appData ->
-                    showRecipe(appData)
-                }
-        }
-    }
-}
