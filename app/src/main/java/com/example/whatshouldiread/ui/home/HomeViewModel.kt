c91380694c030b56901c7d07405ac7a28cd988ec
Minh-Dao Vu <vumd@mail.uc.edu>
2020-04-05 19:09:07 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 96eb740a68ac60248c99ef0e0d5d6d4667225e31, reversing
    changes made to 912c31bc1719b4bb194fdf99570cd4fb392d02ad.

--- app/src/main/java/com/example/whatshouldiread/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/home/HomeViewModel.kt
@@ -13,3 +13,32 @@ class HomeViewModel : ViewModel() {
 }
 
 //Here, a method could be added from the viewModel
+
+class MyViewModel : ViewModel() {
+    private val users: MutableLiveData<List<User>> by lazy {
+        MutableLiveData().also {
+            loadUsers()
+        }
+    }
+
+    fun getUsers(): LiveData<List<User>> {
+        return users
+    }
+
+    private fun loadUsers() {
+        // Do an asynchronous operation to fetch users.
+    }
+}
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    // Create a ViewModel the first time the system calls an activity's onCreate() method.
+    // Re-created activities receive the same MyViewModel instance created by the first activity.
+
+    // Use the 'by viewModels()' Kotlin property delegate
+    // from the activity-ktx artifact
+    val model: MyViewModel by viewModels()
+    model.getUsers().observe(this, Observer<List<User>>{ users ->
+        // update UI
+    })
+}
+}
