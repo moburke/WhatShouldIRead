d3d744300cf3f9c46e7aa462203794e8abe9ad05
Minh-Dao Vu <vumd@mail.uc.edu>
2020-04-24 16:50:14 -0400

    Revert "Added 3-10 question strings for the ten different answer"
    This reverts commit 12f92926a7d76b49b04642b815d4f97da44b8b24.

--- app/src/main/java/com/example/whatshouldiread/customclasses/Question.java
+++ app/src/main/java/com/example/whatshouldiread/customclasses/Question.java
@@ -11,14 +11,6 @@ public class Question {
     private int questionPosition;
     private String questionAnswer1;
     private String questionAnswer2;
-    private String questionAnswer3;
-    private String questionAnswer4;
-    private String questionAnswer5;
-    private String questionAnswer6;
-    private String questionAnswer7;
-    private String questionAnswer8;
-    private String questionAnswer9;
-    private String questionAnswer10;
 
     /*
      * Default constructor for Question. Sets default text and position of 1.
@@ -28,20 +20,8 @@ public class Question {
     {
         this.questionText = "TestQuestionText";
         this.questionPosition = 1;
-        this.questionPosition = 2;
-        this.questionPosition = 3;
-        this.questionPosition = 4;
-        this.questionPosition = 5;
-        this.questionAnswer1 = "Modern";
-        this.questionAnswer2 = "Old";
-        this.questionAnswer3 = "One-Off";
-        this.questionAnswer4 = "Series";
-        this.questionAnswer5 = "Long";
-        this.questionAnswer6 = "Short";
-        this.questionAnswer7 = "Fiction";
-        this.questionAnswer8 = "Non-Fiction";
-        this.questionAnswer9 = "English";
-        this.questionAnswer10 = "Non-English";
+        this.questionAnswer1 = "A";
+        this.questionAnswer2 = "B";
     }
 
     /*
@@ -59,7 +39,6 @@ public class Question {
         this.questionPosition = questionPosition;
         this.questionAnswer1 = questionAnswer1;
         this.questionAnswer2 = questionAnswer2;
-        this.questionAnswer3 = questionAnswer3;
     }
 
     /*
@@ -124,53 +103,4 @@ public class Question {
     {
         this.questionAnswer2 = questionAnswer2;
     }
-
-    public String getQuestionAnswer3(){return questionAnswer3;}
-
-    public void setQuestionAnswer3(String questionAnswer3){
-        this.questionAnswer3 = questionAnswer3;
-    }
-
-    public String getQuestionAnswer4(){return questionAnswer4;}
-
-    public void setQuestionAnswer4(String questionAnswer4){
-        this.questionAnswer4 = questionAnswer4;
-    }
-
-    public String getQuestionAnswer5(){return questionAnswer5;}
-
-    public void setQuestionAnswer5(String questionAnswer5){
-        this.questionAnswer5 = questionAnswer5;
-    }
-
-    public String getQuestionAnswer6(){return questionAnswer6;}
-
-    public void setQuestionAnswer6(String questionAnswer6){
-        this.questionAnswer6 = questionAnswer6;
-    }
-
-    public String getQuestionAnswer7(){return questionAnswer7;}
-
-    public void setQuestionAnswer7(String questionAnswer7){
-        this.questionAnswer7 = questionAnswer7;
-    }
-
-    public String getQuestionAnswer8(){return questionAnswer8;}
-
-    public void setQuestionAnswer8(String questionAnswer8){
-        this.questionAnswer8 = questionAnswer8;
-    }
-
-    public String getQuestionAnswer9(){return  questionAnswer9;}
-
-    public void setQuestionAnswer9(String questionAnswer9){
-        this.questionAnswer9 = questionAnswer9;
-    }
-
-    public String getQuestionAnswer10(){return questionAnswer10;}
-
-    public void setQuestionAnswer10(String questionAnswer10){
-        this.questionAnswer10 = questionAnswer10;
-    }
-
 }
