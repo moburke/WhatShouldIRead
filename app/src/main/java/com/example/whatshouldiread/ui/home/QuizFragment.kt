355715ae9dcfc41fdde4bf311ff97c078583ea5f
Minh-Dao Vu <vumd@mail.uc.edu>
2020-04-24 17:24:09 -0400

    Update quizfragment.xml, revert Kotlin file to original implementation

--- app/src/main/java/com/example/whatshouldiread/ui/home/QuizFragment.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/home/QuizFragment.kt
@@ -7,34 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
 import com.example.whatshouldiread.R
-import com.example.whatshouldiread.customclasses.Book
-import com.example.whatshouldiread.customclasses.Question
-import kotlinx.android.synthetic.main.quiz_fragment.*
 
-//class QuizFragment : Fragment() {
+class QuizFragment : Fragment() {
 
-    //companion object {
-      //  fun newInstance() = QuizFragment()
-    //}
+    companion object {
+        fun newInstance() = QuizFragment()
+    }
 
-    //private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
 
-    //override fun onCreateView(
-      //  inflater: LayoutInflater,
-        //container: ViewGroup?,
-        //savedInstanceState: Bundle?
-    //) {
-      //  super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.quiz_fragment, container, false)
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
 
-
-
-    //}
-
-    //override fun onActivityCreated(savedInstanceState: Bundle?) {
-      //  super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-    //}
-//}
+}
