ee4657fccbc4321d18554c216adef3cb9cd089c8
Minh-Dao Vu <vumd@mail.uc.edu>
2020-04-24 18:40:39 -0400

    Update QuizFragment, add in functionality

--- app/src/main/java/com/example/whatshouldiread/ui/home/QuizFragment.kt
+++ app/src/main/java/com/example/whatshouldiread/ui/home/QuizFragment.kt
@@ -1,13 +1,14 @@
 package com.example.whatshouldiread.ui.home
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.whatshouldiread.R
+import com.example.whatshouldiread.customclasses.Quiz
+import kotlinx.android.synthetic.main.quiz_fragment.*
 
 class QuizFragment : Fragment() {
 
@@ -16,11 +17,35 @@ class QuizFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var quiz = Quiz()
+    private var position = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
+        var initialQuestion = quiz.questions.get(0)
+        txtQuestionText.text = initialQuestion.questionText
+        btnAnswerA.text = initialQuestion.questionAnswer1
+        btnAnswerB.text = initialQuestion.questionAnswer2
+
+        btnAnswerA.setOnClickListener {
+            quiz.answers.set(position, 0)
+            position++
+            var question = quiz.questions.get(position)
+            txtQuestionText.text = question.questionText
+            btnAnswerA.text = question.questionAnswer1
+            btnAnswerB.text = question.questionAnswer2
+        }
+        btnAnswerB.setOnClickListener {
+            quiz.answers.set(position, 1)
+            position++
+            var question = quiz.questions.get(position)
+            txtQuestionText.text = question.questionText
+            btnAnswerA.text = question.questionAnswer1
+            btnAnswerB.text = question.questionAnswer2
+        }
         return inflater.inflate(R.layout.quiz_fragment, container, false)
     }
 
